d8dcc23ad470f8fb540c137ad1c1ff80b83cb76a
bloshound <bloshound@gmail.com>
2021-03-13 19:10:14 +0300

    !

--- src/main/java/com/example/sweater/AppController.java
+++ src/main/java/com/example/sweater/AppController.java
@@ -4,11 +4,7 @@ import com.example.sweater.domain.Message;
 import com.example.sweater.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -18,23 +14,19 @@ public class AppController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting1")// адрес после URL кудв мепится запрос
-    public String greeting1(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
-                            @RequestParam(value = "age", required = false, defaultValue = "10") int age,
-                            Map<String, Object> model) {
-        model.put("name", name);
-        model.put("age", age);
-        return "greeting1";// возвращаемый темплайт шаблон ( в данном случае mustache)
+    @GetMapping("/")// адрес после URL кудв мепится запрос
+    public String greeting(Map<String, Object> model) {
+        return "greeting";// возвращаемый темплайт шаблон ( в данном случае mustache)
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam("text") String text,
                       @RequestParam("tag") String tag,
                       Map<String, Object> model) {
