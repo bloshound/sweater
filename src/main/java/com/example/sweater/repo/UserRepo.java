632c7515a6cc838708ba95406a63cf7372ed708e
bloshound <bloshound@gmail.com>
2021-03-14 11:11:44 +0300

    111

--- src/main/java/com/example/sweater/repo/UserRepo.java
+++ src/main/java/com/example/sweater/repo/UserRepo.java
@@ -4,4 +4,5 @@ import com.example.sweater.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
